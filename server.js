dc38855d788126b5ede0d5722729f4b8a44d90de
Pradeep Jagannathan <n.janapradeep@gmail.com>
2025-07-15 14:51:13 -0400

    completed stage02 add getCustomer

--- server.js
+++ server.js
@@ -4,9 +4,16 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 4000;
 
+const da = require("./data-access");
+
 // Serve static files from the "public" directory
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
+});
+
+app.get('/customers', async (req, res) => {
+    const customers = await da.getCustomers();
+    res.send(customers);
 });
